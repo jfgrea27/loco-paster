46f68a9e463c3717eec9c86a1f20f3476a0a97ae
James Rea <jamesrea97@gmail.com>
2024-04-29 12:26:09 +0100

    Initial commit.
    - Add `flake.nix` and `.envrc`.
    - Add `.gitignore`.

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A flake for loco-paster.";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        p = import nixpkgs {
+          inherit system;
+        };
+      in
+        {
+          devShells = rec {
+            default = nixpkgs.legacyPackages.${system}.mkShell {
+              packages = [
+                # golang specific
+                p.go
+                p.gopls
+                p.go-tools
+                p.errcheck
+                p.gofumpt
+              ];
+              shellHook=''
+                export GOROOT=${p.go.outPath}/share/go
+                unset GOPATH
+          '';
+            };
+          };
+        }
+    );
+}
